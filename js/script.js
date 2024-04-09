80c732181a9242be90da69e58ec67e6a5b58b087
Rosa Jelves <rosa.sakura.j@gmail.com>
2024-04-09 09:47:49 -0400

    solución despliegue navbar

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
